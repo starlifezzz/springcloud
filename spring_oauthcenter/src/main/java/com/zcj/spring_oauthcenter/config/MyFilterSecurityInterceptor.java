393b005672e48f5a771fce56c383127a92697608
778280151@qq.com <zcj778280151>
2020-03-10 11:27:20 +0800

    oauth2资源和认证服务器的配置

--- spring_oauthcenter/src/main/java/com/zcj/spring_oauthcenter/config/MyFilterSecurityInterceptor.java
+++ spring_oauthcenter/src/main/java/com/zcj/spring_oauthcenter/config/MyFilterSecurityInterceptor.java
@@ -19,9 +19,10 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
-/**@author mqz77
- * @date  2020.3.10
- * security过滤器
+/**
+ * @author mqz77
+ * @date 2020.3.10
+ * security过滤器(负责接口权限)
  */
 @Configuration
 public class MyFilterSecurityInterceptor extends GenericFilterBean {
@@ -43,11 +44,16 @@ public class MyFilterSecurityInterceptor extends GenericFilterBean {
         System.out.println("This is a filter before UsernamePasswordAuthenticationFilter.");
         // 继续调用 Filter 链
         String name = SecurityContextHolder.getContext().getAuthentication().getName();
-        if ("anonymousUser".equals(name)) {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+        String requestURI = httpServletRequest.getRequestURI();
+//        接口的正则表达式匹配规则
+        String urlMatches = "^/oauth/\\S*";
+        //查看是否登录(因为这个过滤器加在了security过滤链的最后，所以未登录用户是anonymousUser)
+        if ("anonymousUser".equals(name) || requestURI.matches(urlMatches)) {
+            //未登录放行login页面
             filterChain.doFilter(servletRequest, servletResponse);
         } else {
-            HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
-            String requestURI = httpServletRequest.getRequestURI();
+//           已登录查看接口权限
             TbPermissionExample tbPermissionExample = new TbPermissionExample();
             tbPermissionExample.or().andUrlIsNotNull();
             List<TbPermission> tbPermissions = tbPermissionDao.selectByExample(tbPermissionExample);
