196ae3ea67aeb1471c6713e08554a15969a201f8
778280151@qq.com <zcj778280151>
2020-03-16 14:00:36 +0800

    1. oauth2资源和认证服务器的配置,给common和login服务添加拦截（access和filter方式）
    2.oauth2模块添加mybatis plus模块
    
    3.添加mybatis plus代码生成
    next version：

--- spring_oauthcenter/src/main/java/com/zcj/spring_oauthcenter/config/MyBatiaPlusConfig.java
+++ spring_oauthcenter/src/main/java/com/zcj/spring_oauthcenter/config/MyBatiaPlusConfig.java
@@ -2,6 +2,8 @@ package com.zcj.spring_oauthcenter.config;
 
 
 import com.alibaba.druid.pool.DruidDataSource;
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.pagination.optimize.JsqlParserCountOptimize;
 import com.baomidou.mybatisplus.extension.spring.MybatisSqlSessionFactoryBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -36,6 +38,18 @@ public class MyBatiaPlusConfig {
         sqlSessionFactoryBean.setConfigLocation(mybatisConfigXml);
         sqlSessionFactoryBean.setTypeAliasesPackage("com.zcj.spring_oauthcenter.po");
         return sqlSessionFactoryBean;
-
     }
+
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
+        // 设置请求的页面大于最大页后操作， true调回到首页，false 继续请求  默认false
+        // paginationInterceptor.setOverflow(false);
+        // 设置最大单页限制数量，默认 500 条，-1 不受限制
+        // paginationInterceptor.setLimit(500);
+        // 开启 count 的 join 优化,只针对部分 left join
+        paginationInterceptor.setCountSqlParser(new JsqlParserCountOptimize(true));
+        return paginationInterceptor;
+    }
+
 }
